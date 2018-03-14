ddd9b1424fb4990a1a25d5355fbc8069dd001525
Yusuf Ozlu <ozluyusuf@gmail.com>
2018-03-14 15:42:30 +0100

    fix eslint

--- app/common/media.js
+++ app/common/media.js
@@ -1,11 +1,11 @@
-import { css } from 'styled-components';
+import { css } from 'styled-components'
 
 const breakpoints = {
   tablet: 600,
   tabletWide: 900,
   desktop: 1200,
-  desktopWide: 1800
-};
+  desktopWide: 1800,
+}
 
 // Iterate through the breakpoints and create a media template
 const mq = Object.keys(breakpoints).reduce((accumulator, label) => {
@@ -13,8 +13,8 @@ const mq = Object.keys(breakpoints).reduce((accumulator, label) => {
     @media (min-width: ${breakpoints[label] / 16}em) {
       ${css(...args)};
     }
-  `;
-  return accumulator;
-}, {});
+  `
+  return accumulator
+}, {})
 
-export { breakpoints, mq };
+export { breakpoints, mq }
